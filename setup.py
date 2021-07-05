f3c746a012dc71262e5654a825b4df8c6a19648a
synw <synwx@protonmail.com>
2021-07-05 12:03:25 +0200

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-version = "0.0.1"
+version = "0.0.2"
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
